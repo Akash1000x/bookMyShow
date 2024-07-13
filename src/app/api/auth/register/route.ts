47bbddf7fdafa265d0084ae87121b3b5c80ace21
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-13 22:17:29 +0530

    Added register route

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export const POST = async (req: Request) => {
+  try {
+    const { firstName, phone, password } = await req.json();
+    console.log(firstName, phone, password);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        firstName,
+        phoneNumber: phone,
+        password: hashedPassword,
+      },
+    });
+    return NextResponse.redirect("/login");
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+    if (error.code === "P2002") {
+      return NextResponse.json({ error: "User already exists" }, { status: 409 });
+    }
+
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+};
