23a0c03c9cfcc366cb98b010999d527fb56179bf
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-13 22:15:12 +0530

    new data for seeding the db

--- prisma/data.ts
+++ prisma/data.ts
@@ -9,11 +9,6 @@ export const hallsData = [
       postalCode: "400001",
       street: "Mumbai Central",
     },
-    movie: {
-      movieName: "Deadpool 3",
-      duration: 180,
-      releaseDate: new Date("2025-07-25"),
-    },
   },
   {
     hallName: "INOX",
@@ -24,11 +19,6 @@ export const hallsData = [
       country: "india",
       postalCode: "302019",
       street: "vivek vihar",
-    },
-    movie: {
-      movieName: "Deadpool 3",
-      duration: 180,
-      releaseDate: new Date("2025-07-25"),
     },
   },
   {
@@ -41,12 +31,6 @@ export const hallsData = [
       postalCode: "302021",
       street: "durgapura",
     },
-    movie: {
-      movieName: "Deadpool 3",
-      duration: 180,
-      releaseDate: new Date("2025-07-25"),
-    },
-    seats: Array.from({ length: 100 }, (_, i) => ({ seatNumber: i + 1 })),
   },
   {
     hallName: "rajmandir",
@@ -58,11 +42,44 @@ export const hallsData = [
       postalCode: "302008",
       street: "mi road",
     },
-    movie: {
-      movieName: "Deadpool 3",
-      duration: 180,
-      releaseDate: new Date("2025-07-25"),
-    },
-    seats: Array.from({ length: 100 }, (_, i) => ({ seatNumber: i + 1 })),
+  },
+];
+
+export const movies = [
+  {
+    movieName: "Deadpool & Wolverine",
+    releaseDate: new Date("2025-07-25"),
+
+    duration: 180,
+    genre: "Action/Adventure/Comedy",
+    imageUrl:
+      "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MjY3LjJLIExpa2Vz,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00341295-ppfuthsqmf-portrait.jpg",
+  },
+  {
+    movieName: "Hindustani 2",
+    releaseDate: new Date("2025-07-05"),
+
+    duration: 180,
+    genre: "Action/Drama/Thriller",
+    imageUrl:
+      "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-NS42LzEwICAzLjRLIFZvdGVz,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00402649-gfrrlpdvkp-portrait.jpg",
+  },
+  {
+    movieName: "Kill",
+    releaseDate: new Date("2025-07-10"),
+
+    duration: 180,
+    genre: "Action/Thriller",
+    imageUrl:
+      "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC45LzEwICAyNEsgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00374797-kqytbtqgwb-portrait.jpg",
+  },
+  {
+    movieName: "Kalki 2898 AD",
+    releaseDate: new Date("2025-06-25"),
+
+    duration: 180,
+    genre: "Action/Sci-Fi/Thriller",
+    imageUrl:
+      "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC43LzEwICA2MjcuMUsgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00352941-qdafpgypxr-portrait.jpg",
   },
 ];
