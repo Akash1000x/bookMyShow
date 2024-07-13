68c65f2f786ccb4d3983ec8114ae71277c0daece
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-13 22:18:21 +0530

    Add hostname for images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets-in.bmscdn.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
