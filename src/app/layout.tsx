3cdae825236eec8c3c18ef10d0556fdc025a3eaa
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-14 23:27:26 +0530

    update global layout.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Providers from "@/Providers";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
