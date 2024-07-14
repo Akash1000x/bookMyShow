ff1a0e76a96d1d6f8a29cf718778969f76419cc7
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-15 00:05:39 +0530

    update buy-ticket/hallId

--- src/app/buy-ticket/[...hall]/page.tsx
+++ src/app/buy-ticket/[...hall]/page.tsx
@@ -1,5 +1,5 @@
 import { getMovieHallWithSeat } from "@/actions/addNewUser";
-import Bookseets from "@/components/Bookseets";
+import BookSeets from "@/components/Bookseets";
 import Container from "@/components/ui/container";
 import Wrapper from "@/components/ui/wrapper";
 import prisma from "@/db";
@@ -12,7 +12,7 @@ export default async function Page({ params }: { params: { hall: string[] } }) {
   return (
     <Wrapper>
       <Container>
-        <Bookseets hall={hall} movieId={params.hall[2]} />
+        <BookSeets hall={hall} movieId={params.hall[2]} />
       </Container>
     </Wrapper>
   );
