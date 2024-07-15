dc3209f97cc753dfaa70533d8327bb191459d4b7
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-16 00:24:25 +0530

    fix type of BookSeats

---
+++ src/components/BookSeats.tsx
@@ -0,0 +1,66 @@
+"use client";
+import * as React from "react";
+import { Button } from "./ui/button";
+import Box from "./ui/box";
+import { HallMovie } from "@/types";
+import { useSession } from "next-auth/react";
+import { bookseets, getMovieHallWithSeat } from "@/actions/action";
+import { hallMovie } from "@prisma/client";
+import { TicketConfirm } from "./TicketConfirm";
+
+export default function Bookseets({ hall, movieId }: { hall: HallMovie | null; movieId: string }) {
+  const [selectedSeets, setSelectedSeets] = React.useState<number[]>([]);
+  const [hallData, sethallData] = React.useState<HallMovie | null>();
+  const [openDialog, setOpenDialog] = React.useState<boolean>(false);
+  const session = useSession();
+
+  React.useEffect(() => {
+    sethallData(hall);
+  }, [hall]);
+
+  const selectSeet = (seatId: number) => {
+    if (selectedSeets.includes(seatId)) {
+      setSelectedSeets((prev) => prev.filter((n) => n != seatId));
+    } else {
+      setSelectedSeets([...selectedSeets, seatId]);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={() => setOpenDialog(!openDialog)}>open dialog</Button>
+      <div>
+        {hallData &&
+          hallData.Seat &&
+          hallData.Seat.sort((a, b) => a.seatNumber - b.seatNumber).map((value, i) => (
+            <Box
+              key={i}
+              className={`m-2 ${selectedSeets.includes(value.id) && "bg-green-500 text-white"}`}
+              disabled={!value.available}
+              onClick={() => selectSeet(value.id)}
+            >
+              {value.seatNumber}
+            </Box>
+          ))}
+      </div>
+      <Button
+        variant={"default"}
+        onClick={async () => {
+          if (hallData?.id !== undefined && !!session.data?.user?.id) {
+            await bookseets(hallData.hallId, Number(movieId), session.data?.user?.id, selectedSeets, hallData.id).then(
+              async () => {
+                let data: hallMovie | null = await getMovieHallWithSeat(hallData.hallId, Number(movieId));
+                setOpenDialog(!openDialog);
+                sethallData(data);
+                setSelectedSeets([]);
+              },
+            );
+          }
+        }}
+      >
+        book seet
+      </Button>
+      <TicketConfirm open={openDialog} setOpen={() => setOpenDialog(!openDialog)} />
+    </div>
+  );
+}
