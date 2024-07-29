b9cb71267bef282d1de06258108d1fad6597e5cb
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-29 23:26:33 +0530

    remove extera margin in footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-20 flex h-[200px] w-full items-center justify-center bg-black text-4xl font-bold text-white">
+    <footer className="flex h-[200px] w-full items-center justify-center bg-black text-4xl font-bold text-white">
       Developed by
       <Link href="https://akashkumawat.vercel.app" className="pl-4 text-blue-400 hover:underline">
         Akash Kumawat
