32eb67608bb56a32ebc8e50a59d178036154d364
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-30 09:52:32 +0530

    Add signup route

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignUp from "@/components/form/sign-up";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="h-full">
+      <SignUp />
+    </div>
+  );
+}
